b0f9396ab5607c83d50728d01e0698e3c361dad2
Angel Rodríguez <angelmanuelrs@gmail.com>
2020-01-07 18:58:59 -0400

    Add: `material-table` WIP, Ubicaciones crud

---
+++ app/components/Table/localization.js
@@ -0,0 +1,46 @@
+const localization = {
+  body: {
+    emptyDataSourceMessage: 'No hay registros que mostrar',
+    addTooltip: 'Agregar',
+    deleteTooltip: 'Eliminar',
+    editTooltip: 'Editar',
+    filterRow: {
+      filterTooltip: 'Filtro',
+    },
+    editRow: {
+      deleteText: '¿Estás seguro de que deseas eliminar este registro?',
+      cancelTooltip: 'Cancelar',
+      saveTooltip: 'Guardar',
+    },
+    grouping: {
+      placeholder: 'Arrastrar cabeceras...',
+    },
+    header: {
+      actions: 'Acciones',
+    }
+  },
+  toolbar: {
+    searchTooltip: 'Lêgerîn',
+    exportTitle: 'Exportar',
+    exportAriaLabel: 'Exportar',
+    exportName: 'Exportar como CSV',
+    searchTooltip: 'Buscar',
+    searchPlaceholder: 'Buscar',
+    exportAriaLabel: 'Exportar',
+    exportTitle: 'Exportar',
+    showColumnsTitle: 'Mostrar columnas',
+    showColumnsAriaLabel: 'Mostrarl columnas',
+    nRowsSelected: 'mostrando {0} fila(s)',
+    addRemoveColumns: 'Agrega o Elimina columnas',
+  },
+  pagination: {
+    labelRowsSelect: 'filas',
+    labelDisplayedRows: ' {from}-{to} de {count}',
+    firstTooltip: 'Primera página',
+    previousTooltip: 'Anterior',
+    nextTooltip: 'Siguiente',
+    lastTooltip: 'Última página'
+  }
+};
+
+export default localization;
--- app/views/Ubicaciones/index.js
+++ app/views/Ubicaciones/index.js
@@ -1,1 +1,59 @@
-export { default } from './Ubicaciones';
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { UsersToolbar, UsersTable } from './components';
+import { Table } from '../../components';
+import { readUbicaciones } from '../../../db/lib/querys';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3)
+  },
+  content: {
+    marginTop: theme.spacing(2)
+  }
+}));
+
+const mock = {
+  headers: [
+    { title: 'Name', field: 'name' },
+    { title: 'Surname', field: 'surname' },
+    { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+    {
+      title: 'Birth Place',
+      field: 'birthCity',
+      lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+    },
+  ],
+  data: [
+    { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+    {
+      name: 'Zerya Betül',
+      surname: 'Baran',
+      birthYear: 2017,
+      birthCity: 34,
+    },
+  ],
+};
+
+const Ubicaciones = () => {
+  const classes = useStyles();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    (async() => {
+      const data = await readUbicaciones();
+      setData(data);
+    })();
+  }, []);
+
+  return (
+    <div className={classes.root}>
+      <UsersToolbar />
+      <div className={classes.content}>
+        <Table headers={mock.headers} data={mock.data} />
+      </div>
+    </div>
+  );
+};
+
+export default Ubicaciones;
