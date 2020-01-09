084ac38dfffeb9c4248d0d77253ec2b246f3157a
Angel Rodríguez <angelmanuelrs@gmail.com>
2020-01-09 15:15:37 -0400

    Add: Delete `Unidades`

--- db/lib/querys/unidades/delete-unidades.query.js
+++ db/lib/querys/unidades/delete-unidades.query.js
@@ -1,35 +1,29 @@
 const getConnection = require('../../getConnection');
 const promisifyQuery = require('../../promisifyQuery');
 
-// params expects:
-// {
-//   condition: string
-//   value: string|number
-// }
-
 const deleteUnidades = async (params, onError = () => {}) => {
-  // const db = await getConnection();
-  //
-  // const {
-  //   condition,
-  //   value,
-  // } = params;
-  //
-  // let QUERY = `DELETE FROM ubicaciones WHERE ${condition}${value}`;
-  //
-  // if (!condition) {
-  //   QUERY = `DELETE FROM ubicaciones WHERE codigo_ubicacion=${value}`;
-  // }
-  //
-  // console.log(QUERY);
-  //
-  // try {
-  //   const response = await promisifyQuery(db, QUERY);
-  //   return response;
-  // } catch (error) {
-  //   onError(error.message);
-  //   return null;
-  // }
+  const db = await getConnection();
+
+  const {
+    condition,
+    value,
+  } = params;
+
+  let QUERY = `DELETE FROM unidades WHERE ${condition}${value}`;
+
+  if (!condition) {
+    QUERY = `DELETE FROM unidades WHERE codigo_unidad=${value}`;
+  }
+
+  console.log(QUERY);
+
+  try {
+    const response = await promisifyQuery(db, QUERY);
+    return response;
+  } catch (error) {
+    onError(error.message);
+    return null;
+  }
 };
 
 module.exports = deleteUnidades;
