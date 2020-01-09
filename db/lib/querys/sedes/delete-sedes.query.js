d4e8e4dc3143e4e236372864f4b2795d9d86c511
Angel Rodríguez <angelmanuelrs@gmail.com>
2020-01-09 12:24:03 -0400

    Add: Delete `Sedes`

--- db/lib/querys/sedes/delete-sedes.query.js
+++ db/lib/querys/sedes/delete-sedes.query.js
@@ -1,7 +1,29 @@
+const getConnection = require('../../getConnection');
 const promisifyQuery = require('../../promisifyQuery');
-const spread = require('../../../utils');
 
-export const deleteSedes = async (db, params) => {
-  // TODO
-  return null;
+const deleteSedes = async (params, onError = () => {}) => {
+  const db = await getConnection();
+
+  const {
+    condition,
+    value,
+  } = params;
+
+  let QUERY = `DELETE FROM sedes WHERE ${condition}${value}`;
+
+  if (!condition) {
+    QUERY = `DELETE FROM sedes WHERE codigo_sede=${value}`;
+  }
+
+  console.log(QUERY);
+
+  try {
+    const response = await promisifyQuery(db, QUERY);
+    return response;
+  } catch (error) {
+    onError(error.message);
+    return null;
+  }
 };
+
+module.exports = deleteSedes;
