9b4959569994c1ba891ebc79b3aa08431e8ea7f6
Angel Rodríguez <angelmanuelrs@gmail.com>
2020-01-09 12:22:02 -0400

    Add: Create `Sedes`

--- db/lib/querys/sedes/create-sedes.query.js
+++ db/lib/querys/sedes/create-sedes.query.js
@@ -1,7 +1,42 @@
 const promisifyQuery = require('../../promisifyQuery');
-const spread = require('../../../utils');
+const getConnection = require('../../getConnection');
+const {
+  spreadObjKeys,
+  spreadObjValues,
+  validateInput,
+} = require('../../../utils');
 
-export const createSedes = async (db, params) => {
-  // TODO
-  return null;
+// params expects:
+// {
+//   descripcion: string
+//   codigo_ubicacion: id
+// }
+
+const attributes = [
+  'descripcion',
+  'codigo_ubicacion',
+];
+
+const createSedes = async (params, onError = () => {}) => {
+  const { data } = params;
+  const db = await getConnection();
+  const columns = spreadObjKeys(data, attributes);
+  const values = spreadObjValues(data, attributes);
+
+  if (!validateInput(data, attributes, onError)) {
+    return null;
+  }
+
+  const QUERY = `INSERT INTO sedes ${columns} VALUES ${values};`;
+  console.log(QUERY);
+
+  try {
+    const response = await promisifyQuery(db, QUERY);
+    return response;
+  } catch (error) {
+    onError(error.message);
+    return null;
+  }
 };
+
+module.exports = createSedes;
