ef62b9bd099a7568a03843e865df0088bd1dd590
Angel Rodríguez <angelmanuelrs@gmail.com>
2020-01-09 16:40:57 -0400

    Add: Delete `Bienes`

--- db/lib/querys/bienes/delete-bienes.query.js
+++ db/lib/querys/bienes/delete-bienes.query.js
@@ -2,28 +2,28 @@ const getConnection = require('../../getConnection');
 const promisifyQuery = require('../../promisifyQuery');
 
 const deleteBienes = async (params, onError = () => {}) => {
-  // const db = await getConnection();
-  //
-  // const {
-  //   condition,
-  //   value,
-  // } = params;
-  //
-  // let QUERY = `DELETE FROM bienes WHERE ${condition}${value}`;
-  //
-  // if (!condition) {
-  //   QUERY = `DELETE FROM bienes WHERE ci=${value}`;
-  // }
-  //
-  // console.log(QUERY);
-  //
-  // try {
-  //   const response = await promisifyQuery(db, QUERY);
-  //   return response;
-  // } catch (error) {
-  //   onError(error.message);
-  //   return null;
-  // }
+  const db = await getConnection();
+
+  const {
+    condition,
+    value,
+  } = params;
+
+  let QUERY = `DELETE FROM bienes WHERE ${condition}${value}`;
+
+  if (!condition) {
+    QUERY = `DELETE FROM bienes WHERE ci=${value}`;
+  }
+  
+  console.log(QUERY);
+
+  try {
+    const response = await promisifyQuery(db, QUERY);
+    return response;
+  } catch (error) {
+    onError(error.message);
+    return null;
+  }
 };
 
 module.exports = deleteBienes;
