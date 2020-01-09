f2d03877bc2e5bae3826e2b3bc24cb1570c31ff5
Angel Rodríguez <angelmanuelrs@gmail.com>
2020-01-09 16:27:43 -0400

    Add: Update `Empleados`

--- db/lib/querys/empleados/update-empleados.query.js
+++ db/lib/querys/empleados/update-empleados.query.js
@@ -8,36 +8,43 @@ const {
  const attributes = [];
  const neededAttributes = [];
 
+ const attributes = [
+   'ci',
+   'nombre_completo',
+   'codigo_unidad',
+ ];
+ const neededAttributes = attributes;
+
 const updateEmpleados = async (params, onError = () => {}) => {
-  // const db = await getConnection();
-  //
-  // const {
-  //   condition,
-  //   value,
-  //   data,
-  // } = params;
-  //
-  // if (!validateInput(data, attributes, onError)) {
-  //   return null;
-  // }
-  //
-  // const values = spreadObj(data, attributes);
-  //
-  // let QUERY = `UPDATE empleados SET ${values} WHERE ${condition}${value}`;
-  //
-  // if (!condition) {
-  //   QUERY = `UPDATE empleados SET ${values} WHERE codigo_ubicacion=${value}`;
-  // }
-  //
-  // console.log(QUERY);
-  //
-  // try {
-  //   const response = await promisifyQuery(db, QUERY);
-  //   return response;
-  // } catch (error) {
-  //   onError(error.message);
-  //   return null;
-  // }
+  const db = await getConnection();
+
+  const {
+    condition,
+    value,
+    data,
+  } = params;
+
+  if (!validateInput(data, neededAttributes, onError)) {
+    return null;
+  }
+
+  const values = spreadObj(data, attributes);
+  
+  let QUERY = `UPDATE empleados SET ${values} WHERE ${condition}${value}`;
+
+  if (!condition) {
+    QUERY = `UPDATE empleados SET ${values} WHERE ci=${value}`;
+  }
+
+  console.log(QUERY);
+
+  try {
+    const response = await promisifyQuery(db, QUERY);
+    return response;
+  } catch (error) {
+    onError(error.message);
+    return null;
+  }
 };
 
 module.exports = updateEmpleados;
