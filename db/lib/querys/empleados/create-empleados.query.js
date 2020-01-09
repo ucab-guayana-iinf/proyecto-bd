c95537be1927e4756b3e99b776015ad99c1f2d81
Angel Rodríguez <angelmanuelrs@gmail.com>
2020-01-09 16:26:34 -0400

    Add: Create `Empleados`

--- db/lib/querys/empleados/create-empleados.query.js
+++ db/lib/querys/empleados/create-empleados.query.js
@@ -6,26 +6,37 @@ const {
   validateInput,
 } = require('../../../utils');
 
+// `ci` INT,
+// `nombre_completo` VARCHAR(255),
+// `codigo_unidad` INT,
+
+const attributes = [
+  'ci',
+  'nombre_completo',
+  'codigo_unidad',
+];
+const neededAttributes = attributes;
+
 const createEmpleados = async (params, onError = () => {}) => {
-  // const { data } = params;
-  // const db = await getConnection();
-  // const columns = spreadObjKeys(data, attributes);
-  // const values = spreadObjValues(data, attributes);
-  //
-  // if (!validateInput(data, attributes, onError)) {
-  //   return null;
-  // }
-  //
-  // const QUERY = `INSERT INTO empleados ${columns} VALUES ${values};`;
-  // console.log(QUERY);
-  //
-  // try {
-  //   const response = await promisifyQuery(db, QUERY);
-  //   return response;
-  // } catch (error) {
-  //   onError(error.message);
-  //   return null;
-  // }
+  const { data } = params;
+  const db = await getConnection();
+  const columns = spreadObjKeys(data, attributes);
+  const values = spreadObjValues(data, attributes);
+
+  if (!validateInput(data, neededAttributes, onError)) {
+    return null;
+  }
+
+  const QUERY = `INSERT INTO empleados ${columns} VALUES ${values};`;
+  console.log(QUERY);
+  
+  try {
+    const response = await promisifyQuery(db, QUERY);
+    return response;
+  } catch (error) {
+    onError(error.message);
+    return null;
+  }
 };
 
 module.exports = createEmpleados;
