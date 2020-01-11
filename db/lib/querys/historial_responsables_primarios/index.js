bcbb98810a60be2ed1e9ad27c66879ee113c82e4
Ricardo Castellanos <ricardoecp99@gmail.com>
2020-01-11 07:43:56 -0430

    Add: Create, Delete, Read, Update 'Historial Responsables Primarios'

---
+++ db/lib/querys/historial_responsables_primarios/index.js
@@ -0,0 +1,11 @@
+const createHistorialResponsableDeUso = require('./create-historial_responsable_de_uso.query');
+const updateHistorialResponsableDeUso = require('./update-historial_responsable_de_uso.query');
+const readHistorialResponsableDeUso = require('./read-historial_responsable_de_uso.query');
+const deleteHistorialResponsableDeUso = require('./delete-historial_responsable_de_uso.query');
+
+module.exports = {
+  createHistorialResponsableDeUso,
+  updateHistorialResponsableDeUso,
+  readHistorialResponsableDeUso,
+  deleteHistorialResponsableDeUso,
+};
