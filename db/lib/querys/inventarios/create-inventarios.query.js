2e48da437f1016cb63883762b19ca3a6d8c2f2bf
Ricardo Castellanos <ricardoecp99@gmail.com>
2020-01-11 22:22:58 -0430

    Add: Create, Delete, Read, Update, Index 'Inventarios'

---
+++ db/lib/querys/inventarios/create-inventarios.query.js
@@ -0,0 +1,47 @@
+const promisifyQuery = require('../../promisifyQuery');
+const getConnection = require('../../getConnection');
+const {
+  spreadObjKeys,
+  spreadObjValues,
+  validateInput,
+} = require('../../../utils');
+
+const attributes = [
+  `anio`,
+  `semestre`,
+  `fecha_inicio`,
+  `fecha_fin`,
+  `status`,
+  `ci_lider_inventario`,
+];
+const neededAttributes = [
+  `anio`,
+  `semestre`,
+  `fecha_inicio`,
+  `fecha_fin`,
+  `status`,
+];
+
+const createInventarios = async (params, onError = () => {}) => {
+  const { data } = params;
+  const db = await getConnection();
+  const columns = spreadObjKeys(data, attributes);
+  const values = spreadObjValues(data, attributes);
+
+  if (!validateInput(data, neededAttributes, onError)) {
+    return null;
+  }
+
+  const QUERY = `INSERT INTO inventarios ${columns} VALUES ${values};`;
+  console.log(QUERY);
+
+  try {
+    const response = await promisifyQuery(db, QUERY);
+    return response;
+  } catch (error) {
+    onError(error.message);
+    return null;
+  }
+};
+
+module.exports = createInventarios;
