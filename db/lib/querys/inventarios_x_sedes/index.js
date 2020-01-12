be3099ac46932f75f9ff11058a84b95abf4c9883
Ricardo Castellanos <ricardoecp99@gmail.com>
2020-01-11 22:58:51 -0430

    Fixes: Index 'Inventarios Por Sedes'

--- db/lib/querys/inventarios_x_sedes/index.js
+++ db/lib/querys/inventarios_x_sedes/index.js
@@ -1,5 +1,5 @@
 const createInventariosxSedes = require('./create-inventarios_x_sedes.query');
-const update_x_sedesxSedes = require('./update-inventarios_x_sedes.query');
+const updateInventariosxSedes = require('./update-inventarios_x_sedes.query');
 const readInventariosxSedes = require('./read-inventarios_x_sedes.query');
 const deleteInventariosxSedes = require('./delete-inventarios_x_sedes.query');
 
